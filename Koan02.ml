ae934ff38da15309bd55835f9bd5f60a22ad1f70
Maggie Zirnhelt <maggiezirnhelt@er-10-131-131-196.wireless.umn.edu>
2019-03-14 13:27:54 -0500

    koan01

---
+++ Koan02.ml
@@ -0,0 +1,16 @@
+(* string bois *)
+
+
+(* string stuff doesnt belong here *)
+
+(* words can be be put together as well. *)
+let stringadd =
+  "this " ^ "is " ^ "true" = _
+
+(* but once put together, a word isn't simple to adjust. *)
+(* String.sub string start length *)
+let str = 
+  "true this is as well"
+
+let stringsub =
+  "true" = _
